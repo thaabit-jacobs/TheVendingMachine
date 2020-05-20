a850c4e890c1d761aa9009e2ba594d7605d63367
Mohammed Thaabit <51443211+thaabit-jacobs@users.noreply.github.com>
2020-05-20 12:30:49 +0200

    added buy() and addstock()
    added an overloaded buy method thats takes a product type as an argument and an int
    added an overloaded addstock method that takes a product type as an argument and an int

--- src/vending/OverloadedVendingMachine.java
+++ src/vending/OverloadedVendingMachine.java
@@ -37,25 +37,27 @@ public class OverloadedVendingMachine {
 	}
 	
 	public void buy(SoftDrink softDrink) {
-		if(softDrinkQty <= 0) {
+		if(softDrinkQty == 0) {
 			System.out.println("SoftDrink Out of stock");
 		} else
 			softDrinkQty--;
 	}
 	
 	public void buy(SaltySnack saltySnack) {
-		if(saltySnacksQty <= 0) {
+		if(saltySnacksQty == 0) {
 			System.out.println("Salty snack Out of stock");
 		} else
 			saltySnacksQty--;
 	}
 	
 	public void buy(Chocolate chocolate) {
-		if(chocolatesQty <= 0) {
+		if(chocolatesQty == 0) {
 			System.out.println("Chocolate Out of stock");
 		} else
 			chocolatesQty--;
 	}
+	
+	
 	
 	public void buy(Product product) {
 		buy(new SoftDrink("", 0.00));
@@ -63,10 +65,38 @@ public class OverloadedVendingMachine {
 		buy(new Chocolate("", 0.00));
 	}
 	
+	public void buy(Product product, int amount) {
+		if(product instanceof SoftDrink) {
+			if(softDrinkQty == 0) 
+				System.out.println("softDrink Out of stock");
+			else if(softDrinkQty < amount)
+				System.out.println("only " + softDrinkQty + " in stock");
+			else
+				softDrinkQty -= amount;
+			
+		} else if(product instanceof SaltySnack) {
+			if(saltySnacksQty == 0) 
+				System.out.println("saltySnacks Out of stock");
+			else if(saltySnacksQty < amount)
+				System.out.println("only " + saltySnacksQty + " in stock");
+			else
+				saltySnacksQty -= amount;
+			
+		} else if(product instanceof Chocolate) {
+			if(chocolatesQty == 0) 
+				System.out.println("chocolates Out of stock");
+			else if(chocolatesQty < amount)
+				System.out.println("only " + chocolatesQty + " in stock");
+			else
+				chocolatesQty -= amount;
+		} 
+		
+	}
+
 	public void addStock(SoftDrink softDrink) {
 		softDrinkQty++;
 	}
-	
+
 	public void addStock(SaltySnack saltySnack) {
 		saltySnacksQty++;
 	}
@@ -79,5 +109,51 @@ public class OverloadedVendingMachine {
 		softDrinkQty += 3;
 		saltySnacksQty += 3;
 		chocolatesQty += 3;
+	}
+	
+	public void addStock(Product product, int amount) {
+		if(product instanceof SoftDrink)
+			softDrinkQty += amount;
+		
+		else if (product instanceof SaltySnack)
+			saltySnacksQty += amount;
+		
+		else if(product instanceof Chocolate)
+			chocolatesQty += amount;
+		
+	}
+	
+	public static void main(String[] args) {
+		OverloadedVendingMachine olvm = new OverloadedVendingMachine(15, 15, 10);
+		Product softdrink = new SoftDrink("softdrink", 12.00);
+		Product saltysnack = new SaltySnack("saltysnack", 18.99);
+		Product chocolate = new Chocolate("chocolate", 24.50);
+		
+		System.out.println(softdrink.description());
+		System.out.println(saltysnack.description());
+		System.out.println(chocolate.description());
+		
+		olvm.buy((Chocolate)chocolate);
+		olvm.buy((SaltySnack)saltysnack);
+		olvm.buy(softdrink, 4);
+		
+		System.out.println(olvm.getStock());
+		
+		olvm.buy(chocolate, 2);
+		olvm.buy((SaltySnack)saltysnack);
+		olvm.buy(softdrink, 4);
+		
+		System.out.println(olvm.getStock((SaltySnack)saltysnack));
+		System.out.println(olvm.getStock((SoftDrink)softdrink));
+		
+		olvm.addStock(softdrink, 5);
+		
+		olvm.buy((SaltySnack)saltysnack);
+		olvm.buy(chocolate, 2);
+		olvm.buy(softdrink, 4);
+		olvm.buy(saltysnack, 4);
+		
+		olvm.addStock(softdrink, 3);
+		olvm.addStock(saltysnack, 3);
 	}
 }
