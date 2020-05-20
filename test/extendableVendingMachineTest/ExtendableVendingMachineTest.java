717a19354a23300fe7123ab8ec5918629b116d44
Mohammed Thaabit <51443211+thaabit-jacobs@users.noreply.github.com>
2020-05-20 13:45:15 +0200

    tested addstock method

--- test/extendableVendingMachineTest/ExtendableVendingMachineTest.java
+++ test/extendableVendingMachineTest/ExtendableVendingMachineTest.java
@@ -32,5 +32,54 @@ class ExtendableVendingMachineTest {
 		ExtendableVendingMachine etvm = new ExtendableVendingMachine(5, 7, 2);
 		assertEquals(14, etvm.getStock(new Product("", 0.00)));
 	}
+	
+	@Test
+	void buySoftDrink() {
+		ExtendableVendingMachine etvm = new ExtendableVendingMachine(5, 7, 2);
+		Product sd = new SoftDrink("", 0.00);
+		etvm.buy(sd);
+		assertEquals(4, etvm.getStock(sd));
+	}
+	
+	@Test
+	void buySaltySnack() {
+		ExtendableVendingMachine etvm = new ExtendableVendingMachine(5, 7, 2);
+		Product ss = new SaltySnack("", 0.00); 
+		etvm.buy(ss);
+		assertEquals(6, etvm.getStock(ss));
+	}
+	
+	@Test
+	void buyChocolate() {
+		ExtendableVendingMachine etvm = new ExtendableVendingMachine(5, 7, 2);
+		Product choc = new Chocolate("", 0.00);
+		etvm.buy(choc);
+		assertEquals(1, etvm.getStock(choc));
+	}
 
+	@Test
+	void addStockSoftDrink() {
+		ExtendableVendingMachine etvm = new ExtendableVendingMachine(5, 7, 2);
+		Product sd = new SoftDrink("", 0.00);
+		etvm.addStock(sd);
+		assertEquals(8, etvm.getStock((SoftDrink)sd));
+	}
+	
+	@Test
+	void addStockSaltySnack() {
+		ExtendableVendingMachine etvm = new ExtendableVendingMachine(5, 7, 2);
+		Product ss = new SaltySnack("", 0.00);
+		etvm.addStock(ss);
+		assertEquals(10, etvm.getStock((SaltySnack)ss));
+	}
+	
+	@Test
+	void addStockChoclate() {
+		ExtendableVendingMachine etvm = new ExtendableVendingMachine(5, 7, 2);
+		Product choc = new Chocolate("", 0.00);
+		etvm.addStock(choc);
+		assertEquals(5, etvm.getStock((Chocolate)choc));
+	}
+	
+	
 }
